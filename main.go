d4ec3cd9506ec684789112d5721890a1e381b6d7
蒼時弦也 <elct9620@frost.tw>
2022-06-15 19:33:24 +0800

    Initialize project

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Static("static"))
+	e.Logger.Fatal(e.Start(":8080"))
+}
