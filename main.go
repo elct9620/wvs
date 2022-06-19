8c53b5b360318498b2f874662e337aa988c6d371
蒼時弦也 <elct9620@frost.tw>
2022-06-19 14:43:36 +0800

    Implement graceful shutdown

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"time"
+
 	"github.com/elct9620/wvs/internal/application"
 	"github.com/elct9620/wvs/internal/infrastructure/container"
 	"github.com/elct9620/wvs/pkg/command"
 	"github.com/elct9620/wvs/pkg/controller"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"golang.org/x/net/context"
 )
 
 func main() {
@@ -21,5 +26,20 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Static("static"))
 	e.GET("/ws", controller.Server)
-	e.Logger.Fatal(e.Start(":8080"))
+
+	var exit chan os.Signal = make(chan os.Signal, 1)
+	go func() {
+		e.Logger.Fatal(e.Start(":8080"))
+	}()
+
+	signal.Notify(exit, os.Interrupt)
+	<-exit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	container.Engine().Stop()
+	container.Hub().Stop()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Error(err)
+	}
 }
