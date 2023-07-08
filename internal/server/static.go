b0eed30ce8ad34113a9f16f026ba4ab6ae5553aa
Aotokitsuruya <contact@aotoki.me>
2023-07-08 17:00:17 +0800

    Simplify assets

--- internal/server/static.go
+++ internal/server/static.go
@@ -11,17 +11,19 @@ import (
 	"go.uber.org/zap"
 )
 
+var BuildOptions = api.BuildOptions{
+	EntryPoints: []string{
+		"assets/app.ts",
+	},
+	Define: map[string]string{
+		"DEV": "true",
+	},
+	Bundle: true,
+	Outdir: "dist",
+}
+
 func WithAssets(logger *zap.Logger) HTTPOptionFn {
-	assets, sse, err := esbuildfs.Serve(api.BuildOptions{
-		EntryPoints: []string{
-			ScriptDir + "/app.ts",
-		},
-		Define: map[string]string{
-			"DEV": "true",
-		},
-		Bundle: true,
-		Outdir: StaticDir + "/js",
-	}, esbuildfs.WithHandlerPrefix("assets"))
+	assets, sse, err := esbuildfs.Serve(BuildOptions, esbuildfs.WithHandlerPrefix("assets"))
 
 	if err != nil {
 		logger.Fatal(err.Error())
